8bea2a16426bff26bffa4e74508e402c432e0638
Victorensta <113982244+Victorensta@users.noreply.github.com>
2023-04-11 21:52:53 +0200

    app etc

--- faces_database.py
+++ faces_database.py
@@ -22,6 +22,8 @@ salman_face_encoding = face_recognition.face_encodings(salman_image)[0]
 ahmed_image = face_recognition.load_image_file("Test_material/Images/AhmedAlotaibi.JPG")
 ahmed_face_encoding = face_recognition.face_encodings(ahmed_image)[0]
 
+thibault_haudricourt_image = face_recognition.load_image_file("Test_material/Images/ThibaultHaudricourt.JPG")
+thibault_haudricourt_face_encoding = face_recognition.face_encodings(thibault_haudricourt_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
