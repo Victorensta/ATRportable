4b3c9ed773b53cc8fb9af447f1fea60947dc5380
Victorensta <113982244+Victorensta@users.noreply.github.com>
2023-04-18 14:32:43 +0200

    multi-cam victor

--- faceRecognitionWebcam_Victor.py
+++ faceRecognitionWebcam_Victor.py
@@ -7,28 +7,32 @@ known_face_encodings = faces_database.known_face_encodings
 known_face_names = faces_database.known_face_names
 
 # Initialize some variables
-face_locations = []
-face_encodings = []
-face_names = []
-process_this_frame = True
+# face_locations = []
+# face_encodings = []
+#
+# process_this_frame = True
 
 
-# capture1 = cv2.VideoCapture(2)#principal
-# capture2 = cv2.VideoCapture('http://172.19.132.92:4747/video')#Victor
+capture1 = cv2.VideoCapture(2)#principal
+capture2 = cv2.VideoCapture('http://172.19.132.92:4747/video')#Victor
 # capture3= cv2.VideoCapture('http://172.19.128.126:4747/video')#Nicolas
 # capture4= cv2.VideoCapture('http://172.19.133.32:4747/video')#Eliot
-capture5=cv2.VideoCapture('http://172.19.128.195:4747/video')#Salman
+# capture5=cv2.VideoCapture('http://172.19.128.195:4747/video')#Salman
 
 
 captures=[
-    # capture1,
-    # capture2,
+    capture1,
+    capture2
+    # ,
     # capture3,
     # capture4
-    capture5
+    # capture5
           ]
 while True:
     for capture in captures:
+        face_locations = []
+        face_encodings = []
+        process_this_frame = True
         frame_width = capture.get(3)
         frame_height = capture.get(4)
         # Grab a single frame of video
@@ -36,8 +40,8 @@ while True:
 
         print(type(frame))
 
-        # if capture!=capture1:
-        frame=cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
+        if capture!=capture1:
+            frame=cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
 
         # Resize frame of video to 1/4 size for faster face recognition processing
         small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
@@ -45,7 +49,8 @@ while True:
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = small_frame
         # [:, :, ::-1]
-        # face_names = []
+        face_names = []
+
         # Only process every other frame of video to save time
         if process_this_frame:
             # Find all the faces and face encodings in the current frame of video
@@ -100,8 +105,6 @@ while True:
 
 
         cv2.imshow(f'{capture}', frame)
-
-
 
         # Hit 'q' on the keyboard to quit!
         if cv2.waitKey(1) & 0xFF == ord('q'):
